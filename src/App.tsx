c46add67b659f2112eedb7251f657a03cfae4a44
Ariovaldo Barbosa Da Silva Junior <ariovaldo.junior@ltm.digital>
2021-08-09 09:36:47 -0300

    cria hook para transacoes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionsModal } from './components/NewTransactionsModal';
-import { TransactionsProvider } from './TransactionsContext';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 Modal.setAppElement('#root');
 
--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,11 +1,11 @@
-import { FormEvent, useContext, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal'
 
+import { useTransactions } from '../../hooks/useTransactions';
 import incomeImg from './../../assets/income.svg';
 import outcomeImg from './../../assets/outcome.svg';
 import { Container, TranscationTypeContainer, RadioBox } from './styles'
 import closeImg from './../../assets/close.svg'
-import { TransactionsContext } from '../../TransactionsContext';
 
 
 interface NewTransactionsModalProps {
@@ -14,7 +14,7 @@ interface NewTransactionsModalProps {
 }
 
 export function NewTransactionsModal({ isOpen, onRequestClose }: NewTransactionsModalProps) {
-  const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions()
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('')
